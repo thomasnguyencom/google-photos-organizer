ff301fd99b1e3749d637ebcf101e4a307ddcd626
thomasnguyencom <contact@thomasnguyen.com>
2022-08-07 11:39:15 -0500

    Not sure what to do after this

--- source/Connect-GPhoto.ps1
+++ source/Connect-GPhoto.ps1
@@ -11,13 +11,12 @@ Begin {
     function Get-GoogleApiCredentials
     {
         Param (
-            $JsonCredentialsFilePath
+            $JsonCredentialsFilePath,
+            [switch] $Output
         )
 
         if(Test-Path -Path $JsonCredentialsFilePath)
         {
-            Write-Host "JSON file found"
-
             $jsonCreds = Get-Content -Path $JsonCredentialsFilePath
         
             $credsJson = $jsonCreds | ConvertFrom-Json
@@ -34,6 +33,19 @@ Begin {
                 RedirectUris = $creds.redirect_uris
                 JavascriptOrigins = $creds.javascript_origins
             }
+
+            if($Output)
+            {
+                Write-Host "JSON file found"
+                Write-Host "  ClientId $($credsObject.ClientId)"
+                Write-Host "  ProjectId $($credsObject.ProjectId)"
+                Write-Host "  AuthUri$($credsObject.AuthUri)"
+                Write-Host "  TokenUri $($credsObject.TokenUri)"
+                Write-Host "  AuthProdviderUrl $($credsObject.AuthProdviderUrl)"
+                Write-Host "  ClientSecret $($credsObject.ClientSecret)"
+                Write-Host "  RedirectUris $($credsObject.RedirectUris)"
+                Write-Host "  JavascriptOrigins $($credsObject.JavascriptOrigins)"
+            }
         
             return $credsObject
         }
@@ -43,29 +55,59 @@ Begin {
         }
     }
     
-    function asdf {
+    function Get-GoogleAuthCode {
+        Param (
+            $Creds,
+            [switch] $Output
+        )
         # https://lazyadmin.nl/it/connect-to-google-api-with-powershell/
     
         # Replace <CLIENT_ID_HERE> with your client id
-        "https://accounts.google.com/o/oauth2/auth?redirect_uri=<CLIENT_ID_HERE>&scope=https://www.googleapis.com/auth/analytics.readonly&approval_prompt=force&access_type=offline"
+        #"https://accounts.google.com/o/oauth2/auth?redirect_uri=$($Creds.ClientId)&scope=https://www.googleapis.com/auth/analytics.readonly&approval_prompt=force&access_type=offline"
     
         # In the comments below, Phani mentioned that the above url is wrong and should be:
-        "https://accounts.google.com/o/oauth2/auth?client_id=<replacemewithclientid>&scope=https://www.googleapis.com/auth/analytics.readonly&response_type=code&redirect_uri=<replacemewithredirecturi>&access_type=offline&approval_prompt=force"
+        $url = "https://accounts.google.com/o/oauth2/auth?client_id=$($Creds.ClientId)&scope=https://www.googleapis.com/auth/analytics.readonly&response_type=code&redirect_uri=$($Creds.RedirectUris)&access_type=offline&approval_prompt=force"
     
+        Write-Host "$($url)"
+
         # I have no time to test it, but if you get an error with the original URL, then check the one mentioned by Phani.
+
+        $url = Read-Host -Prompt "Open Browser to URL above and copy/paste the returned URL after logging in."
+
+        $authCode = $url.Replace('http://localhost/auth/google/callback?code=', '').Replace('&scope=https://www.googleapis.com/auth/analytics.readonly', '')
+
+        if($Output) {
+            Write-Host "URL: $($url)"
+            Write-Host "code: $($authCode)"
+        }
+
+        return $authCode
+    }
     
+    function Connect-GooglePhotosApi {
+        Param (
+            $Creds,
+            [switch] $Output
+        )
+
+        $authCode = Get-GoogleAuthCode -Creds $Creds -Output:$Output
     
         $requestUri = "https://www.googleapis.com/oauth2/v4/token"
         $body = @{
-          code=<authcode>;
-          client_id=<clientId>;
-          client_secret=<clientSecret>;
-          redirect_uri=<redirectUrl>;
-          grant_type="authorization_code"; # Fixed value
+            code = $authCode;
+            client_id = $Creds.ClientId;
+            client_secret = $Creds.ClientSecret;
+            redirect_uri = $Creds.RedirectUris;
+            grant_type = "authorization_code"; # Fixed value
         };
+
+        $body
+
         $tokens = Invoke-RestMethod -Uri $requestUri -Method POST -Body $body;
+
         # Store refreshToken
         Set-Content $PSScriptRoot"\refreshToken.txt" $tokens.refresh_token
+
         # Store accessToken
         Set-Content $PSScriptRoot"\accessToken.txt" $tokens.access_token
     }
@@ -76,14 +118,7 @@ Process {
 
     $creds = (Get-GoogleApiCredentials -JsonCredentialsFilePath $GoogleApiJSONCredentialsFilePath)
 
-    Write-Host "-ClientId $($creds.ClientId)"
-    Write-Host "-ProjectId $($creds.ProjectId)"
-    Write-Host "-AuthUri$($creds.AuthUri)"
-    Write-Host "-TokenUri $($creds.TokenUri)"
-    Write-Host "-AuthProdviderUrl $($creds.AuthProdviderUrl)"
-    Write-Host "-ClientSecret $($creds.ClientSecret)"
-    Write-Host "-RedirectUris $($creds.RedirectUris)"
-    Write-Host "-JavascriptOrigins $($creds.JavascriptOrigins)"
+    $authCode = Connect-GooglePhotosApi -Creds $creds -AuthCode $authCode -Output
 
     Write-Host "----" -ForegroundColor Blue
 }
